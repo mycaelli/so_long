a8a6eec6d3efa5391573709b8f6ac45c8de23bc4
mycaelli <mycaelli@usp.br>
2022-08-05 05:11:38 +0200

    callocing map struct OK + map rows and cols counting OK

--- map.c
+++ map.c
@@ -6,7 +6,7 @@
 /*   By: mcerquei <mcerquei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 05:42:21 by mcerquei          #+#    #+#             */
-/*   Updated: 2022/08/04 21:25:05 by mcerquei         ###   ########.fr       */
+/*   Updated: 2022/08/05 05:10:49 by mcerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,50 +16,31 @@
 #include "./libft/libft.h"
 
 int	line_size(char *line);
-t_map	*map_size(char *file_path);
+t_map	*map_size(char *file_path, t_map *map);
 char	**creat_map();
 
 int	main(int argc, char *argv[])
 {
-	creat_map(map_size(argv[1]));
+	t_map *map;
 
+	map = NULL;
+	map = map_size(argv[1], map);
+	printf("%d\n", map->rows);
+	printf("%d\n", map->cols);	
 	argc++; //ARRUMAR ISSOOOOOO
-	//printf("argc: %d\n", argc);	
 }
-/*
-int	*map_size(char *file_path)
+
+t_map	*map_size(char *file_path, t_map *map)
 {
-	t_map		map;
 	char		*line;
 	int			fd;
 
-	map.sizes[0] = 0;
-	map.sizes[1] = 0;
-	fd = open(file_path, O_RDONLY); //verify fd
-	line = get_next_line(map.fd, 1);
-	while (line[map.sizes[0]] && line[map.sizes[0]] != '\n')
-		map.sizes[0]++;
-	while (line)
-	{
-		line = get_next_line(fd, 1);
-		map.sizes[1]++;
-	}
-	//printf("%d\n", map.sizes[0]);
-	//printf("%d\n", map.sizes[1]);
-	return(map.sizes);
-} */
-
-t_map	*map_size(char *file_path)
-{
-	t_map		*map;
-	char		*line;
-	int			fd;
-
-	//map = ft_calloc(1, sizeof(t_map)); //ver como faz isso
-	//map = ft_calloc(sizeof(t_map), 1);
-	map->rows= 0;
+	map = (t_map*) ft_calloc(1, sizeof(t_map));
+	map->rows = 0;
 	map->cols = 0;
 	fd = open(file_path, O_RDONLY); //verify fd
+	if (fd < 0)
+		printf("ERROR: FD INVALID");
 	line = get_next_line(fd, 1);
 	while (line[map->rows] && line[map->rows] != '\n')
 		map->rows++;
@@ -68,21 +49,29 @@ t_map	*map_size(char *file_path)
 		line = get_next_line(fd, 1);
 		map->cols++;
 	}
-	printf("%d\n", map->rows);
-	printf("%d\n", map->cols);
-
 	return (map);
 } 
 
+/*
 //create map 
 // **game_map
 	// game[0][0] = line[0]
 char	**creat_map(t_map *map)
 {
 	//printf("%d\n", map->rows);
-/*
+
 	map = ft_calloc(sizeof(t_map));
 
-	printf("aqui\n"); */
-	return (map->map_game);
-}
+	printf("aqui\n"); 
+	
+	int i;
+
+	i = 0;
+	char **info = ft_calloc(sizeof(char*), map->rows);
+	while (i < map->rows)
+	{
+		info[i] = ft_calloc(sizeof(char), map->cols);
+	}
+	map->data = info; 
+	return (map->data);
+}*/
