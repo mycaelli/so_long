b3ae8b8da67e3a3eb370baddd0669953bfe568cd
mycaelli <mycaelli@usp.br>
2022-08-05 07:34:54 +0200

    map->data implementation OK

--- map.c
+++ map.c
@@ -6,7 +6,7 @@
 /*   By: mcerquei <mcerquei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 05:42:21 by mcerquei          #+#    #+#             */
-/*   Updated: 2022/08/05 06:40:06 by mcerquei         ###   ########.fr       */
+/*   Updated: 2022/08/05 07:34:00 by mcerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,9 @@
 #include "./includes/gnl.h"
 #include "./libft/libft.h"
 
-int	line_size(char *line);
 void	map_size(char *file_path, t_map *map);
-void	initialize_map(t_map *map);
-char	**creat_map();
+void	map_initialize(t_map *map);
+void	map_generate(char *file_path, t_map *map);
 
 int	main(int argc, char *argv[])
 {
@@ -28,10 +27,12 @@ int	main(int argc, char *argv[])
 	if (!map)
 		printf("MAP ALLOCATION FAILED");
 	map_size(argv[1], map);
-	initialize_map(map);
-	printf("%d\n", map->rows);
-	printf("%d\n", map->cols);
+	map_initialize(map);
+	map_generate(argv[1], map);
+
 	argc++; //ARRUMAR ISSOOOOOO -> if argc == 1 NO VALID MAP WAS GIVEN
+
+	return (0);
 }
 
 void	map_size(char *file_path, t_map *map)
@@ -45,31 +46,51 @@ void	map_size(char *file_path, t_map *map)
 	if (fd < 0)
 		printf("ERROR: FD INVALID");
 	line = get_next_line(fd, 1);
-	while (line[map->rows] && line[map->rows] != '\n')
-		map->rows++;
+	while (line[map->cols] && line[map->cols] != '\n')
+		map->cols++;
 	while (line)
 	{
 		line = get_next_line(fd, 1);
-		map->cols++;
+		map->rows++;
 	}
 }
 
-void	initialize_map(t_map *map)
+void	map_initialize(t_map *map)
 {
-	//char **info;
 	int i;
 	
-	i = 0;
-	//printf("%s\n", map->data);
+	i = -1;
 	map->data = ft_calloc(sizeof(char*), map->rows);
 	if (!map->data)
-		printf("MAP->DATA CALLOC FAILED 1");
-
-	while (i < map->rows)
-	{
+		printf("ERROR: MAP->DATA CALLOC FAILED 1");
+	while (i++ < map->rows)
 		map->data[i] = ft_calloc(sizeof(char), map->cols);
-		i++;
-	}
 	if (!*(map->data))
-		printf("MAP->DATA CALLOC FAILED 2");
+		printf("ERROR: MAP->DATA CALLOC FAILED 2");
+}
+
+void	map_generate(char *file_path, t_map *map)
+{
+	char		*line;
+	int			fd;
+	int			i;
+	int			j;
+	int			count;
+
+	i = 0;
+	j = 0;
+	count = map->rows;
+	fd = open(file_path, O_RDONLY);
+	if (fd < 0)
+		printf("ERROR: FD INVALID");
+	// Tentar fazer verificação dos erros aqui
+	while (count)
+	{
+		line = get_next_line(fd, 1);
+		j = -1;
+		while (j++ < map->cols)
+			map->data[i][j] = line[j];
+		i++;
+		count--;
+	}
 }
