abfb43d3c5c105565a42c5264c97ac8e99c4eebb
mycaelli <mycaelli@usp.br>
2022-08-05 06:40:36 +0200

    removing return value from map_size -> all void now

--- map.c
+++ map.c
@@ -6,7 +6,7 @@
 /*   By: mcerquei <mcerquei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 05:42:21 by mcerquei          #+#    #+#             */
-/*   Updated: 2022/08/05 06:34:59 by mcerquei         ###   ########.fr       */
+/*   Updated: 2022/08/05 06:40:06 by mcerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include "./libft/libft.h"
 
 int	line_size(char *line);
-t_map	*map_size(char *file_path, t_map *map);
+void	map_size(char *file_path, t_map *map);
 void	initialize_map(t_map *map);
 char	**creat_map();
 
@@ -24,27 +24,24 @@ int	main(int argc, char *argv[])
 {
 	t_map *map;
 
-	map = NULL;
-	map = map_size(argv[1], map);
+	map = (t_map*) ft_calloc(1, sizeof(t_map));
+	if (!map)
+		printf("MAP ALLOCATION FAILED");
+	map_size(argv[1], map);
 	initialize_map(map);
-	//printf("%d\n", map->rows);
-	//printf("%d\n", map->cols);	
+	printf("%d\n", map->rows);
+	printf("%d\n", map->cols);
 	argc++; //ARRUMAR ISSOOOOOO -> if argc == 1 NO VALID MAP WAS GIVEN
 }
 
-// callocing map struct
-// getting row and col sizes
-t_map	*map_size(char *file_path, t_map *map)
+void	map_size(char *file_path, t_map *map)
 {
 	char		*line;
 	int			fd;
 
-	map = (t_map*) ft_calloc(1, sizeof(t_map));
-	if (!map)
-		printf("MAP ALLOCATION FAILED");
 	map->rows = 0;
 	map->cols = 0;
-	fd = open(file_path, O_RDONLY); //verify fd
+	fd = open(file_path, O_RDONLY);
 	if (fd < 0)
 		printf("ERROR: FD INVALID");
 	line = get_next_line(fd, 1);
@@ -55,8 +52,7 @@ t_map	*map_size(char *file_path, t_map *map)
 		line = get_next_line(fd, 1);
 		map->cols++;
 	}
-	return (map);
-} 
+}
 
 void	initialize_map(t_map *map)
 {
@@ -77,28 +73,3 @@ void	initialize_map(t_map *map)
 	if (!*(map->data))
 		printf("MAP->DATA CALLOC FAILED 2");
 }
-
-
-/*
-//create map 
-// **game_map
-	// game[0][0] = line[0]
-char	**creat_map(t_map *map)
-{
-	//printf("%d\n", map->rows);
-
-	map = ft_calloc(sizeof(t_map));
-
-	printf("aqui\n"); 
-	
-	int i;
-
-	i = 0;
-	char **info = ft_calloc(sizeof(char*), map->rows);
-	while (i < map->rows)
-	{
-		info[i] = ft_calloc(sizeof(char), map->cols);
-	}
-	map->data = info; 
-	return (map->data);
-}*/
