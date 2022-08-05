b27dc92a6fd3db2823f956e1e770307cf43b21b3
mycaelli <mycaelli@usp.br>
2022-08-05 06:35:18 +0200

    map data calloc OK

--- map.c
+++ map.c
@@ -6,7 +6,7 @@
 /*   By: mcerquei <mcerquei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 05:42:21 by mcerquei          #+#    #+#             */
-/*   Updated: 2022/08/05 05:10:49 by mcerquei         ###   ########.fr       */
+/*   Updated: 2022/08/05 06:34:59 by mcerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 
 int	line_size(char *line);
 t_map	*map_size(char *file_path, t_map *map);
+void	initialize_map(t_map *map);
 char	**creat_map();
 
 int	main(int argc, char *argv[])
@@ -25,17 +26,22 @@ int	main(int argc, char *argv[])
 
 	map = NULL;
 	map = map_size(argv[1], map);
-	printf("%d\n", map->rows);
-	printf("%d\n", map->cols);	
-	argc++; //ARRUMAR ISSOOOOOO
+	initialize_map(map);
+	//printf("%d\n", map->rows);
+	//printf("%d\n", map->cols);	
+	argc++; //ARRUMAR ISSOOOOOO -> if argc == 1 NO VALID MAP WAS GIVEN
 }
 
+// callocing map struct
+// getting row and col sizes
 t_map	*map_size(char *file_path, t_map *map)
 {
 	char		*line;
 	int			fd;
 
 	map = (t_map*) ft_calloc(1, sizeof(t_map));
+	if (!map)
+		printf("MAP ALLOCATION FAILED");
 	map->rows = 0;
 	map->cols = 0;
 	fd = open(file_path, O_RDONLY); //verify fd
@@ -51,6 +57,27 @@ t_map	*map_size(char *file_path, t_map *map)
 	}
 	return (map);
 } 
+
+void	initialize_map(t_map *map)
+{
+	//char **info;
+	int i;
+	
+	i = 0;
+	//printf("%s\n", map->data);
+	map->data = ft_calloc(sizeof(char*), map->rows);
+	if (!map->data)
+		printf("MAP->DATA CALLOC FAILED 1");
+
+	while (i < map->rows)
+	{
+		map->data[i] = ft_calloc(sizeof(char), map->cols);
+		i++;
+	}
+	if (!*(map->data))
+		printf("MAP->DATA CALLOC FAILED 2");
+}
+
 
 /*
 //create map 
