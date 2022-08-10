408c3d63bd235ae05c6ebef6c72edb601f87da19
mycaelli <mycaelli@usp.br>
2022-08-11 00:04:23 +0200

    Valgrind - Invalid write of size X - map_initialize corrected

--- map/map.c
+++ map/map.c
@@ -6,7 +6,7 @@
 /*   By: mcerquei <mcerquei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 05:42:21 by mcerquei          #+#    #+#             */
-/*   Updated: 2022/08/08 01:41:36 by mcerquei         ###   ########.fr       */
+/*   Updated: 2022/08/11 00:01:31 by mcerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,16 +42,19 @@ void	map_size(char *file_path, t_map *map)
 void	map_initialize(t_map *map)
 {
 	int i;
-	
-	i = -1;
+
+	i = 0;
 	map->data = ft_calloc(sizeof(char*), map->rows);
 	if (!map->data)
 	{
 		ft_printf("Error\n %s\n", strerror(ENOMEM));
 		exit (EXIT_FAILURE);
 	}
-	while (i++ < map->rows)
-		map->data[i] = ft_calloc(sizeof(char), map->cols);
+	while (i < map->rows)
+	{
+		map->data[i] = ft_calloc(sizeof(char*), map->cols);
+		i++;
+	}
 	if (!*(map->data))
 	{
 		ft_printf("Error\n %s\n", strerror(ENOMEM));
@@ -79,8 +82,10 @@ void	map_generate(char *file_path, t_map *map)
 		while (pos[1]++ < map->cols)
 		{
 			map->data[pos[0]][pos[1]] = line[pos[1]];
+			printf("%c", map->data[pos[0]][pos[1]]);
 			check_chars(line[pos[1]], check);
 		}
+		printf("\n");
 		pos[0]++;
 		count--;
 	}
