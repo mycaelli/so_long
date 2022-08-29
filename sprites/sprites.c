b86a8e7e7b8bbbf2d03f035135138a0a8481b17c
mycaelli <mycaelli@usp.br>
2022-08-30 00:50:37 +0200

    wall / floor / character sprites OK

--- sprites/sprites.c
+++ sprites/sprites.c
@@ -4,7 +4,7 @@
 
 void img_generate(t_map *map, t_game *game)
 {	
-	void *images[6];
+	void *images[7];
 
 	t_sprite *wall;
 	t_sprite *floor;
@@ -26,18 +26,20 @@ void img_generate(t_map *map, t_game *game)
 		exit (EXIT_FAILURE);
 	}
 	wall->path = "/home/coder/so_long/sprites/images/wall/wall.xpm";
-	floor->path = "/home/coder/so_long/sprites/images/empty_space/floor.xpm";
-	collectible->path = "/home/coder/so_long/sprites/images/collectible/collectible.xpm";
-	character->path = "/home/coder/so_long/sprites/images/character/character.xpm";
+	floor->path = "/home/coder/so_long/sprites/images/floor/floor_24_24.xpm";
+	collectible->path = "/home/coder/so_long/sprites/images/collectible/pumpkin_24_24_certo.xpm";
+	character->path = "/home/coder/so_long/sprites/images/character/ghost_bg.xpm";
 	background->path =  "/home/coder/so_long/sprites/images/background/background.xpm";
+	//door->path = "/home/coder/so_long/sprites/images/exit/AnyConv.com__door_ok.xpm";
 
 	wall->img = mlx_xpm_file_to_image(game->mlx_ptr, wall->path, &wall->width, &wall->height);
 	floor->img = mlx_xpm_file_to_image(game->mlx_ptr, floor->path, &floor->width, &floor->height);
 	character->img = mlx_xpm_file_to_image(game->mlx_ptr, character->path, &character->width, &character->height);
-	collectible->img = mlx_xpm_file_to_image(game->mlx_ptr, collectible->path, &collectible->width, &collectible->height);
-	background->img = mlx_xpm_file_to_image(game->mlx_ptr, background->path, &background->width, &background->height);
-	
-	if (!wall->img || !floor->img || !character->img || !collectible->img || !background->img)
+	//collectible->img = mlx_xpm_file_to_image(game->mlx_ptr, collectible->path, &collectible->width, &collectible->height);
+	//background->img = mlx_xpm_file_to_image(game->mlx_ptr, background->path, &background->width, &background->height);
+	//door->img = mlx_xpm_file_to_image(game->mlx_ptr, door->path, &door->width, &door->height);
+
+	if (!wall->img || !floor->img || !character->img)
 	{
 		ft_printf("Error\n %s\n", strerror(ENOMEM));
 		printf("AQUI\n");
@@ -45,9 +47,10 @@ void img_generate(t_map *map, t_game *game)
 	} 
 	images[0] = wall->img;
 	images[1] = floor->img;
-	images[2] = collectible->img;
-	images[3] = background->img;
+	//images[2] = collectible->img;
+	//images[3] = door->img;
 	images[4] = character->img;
+	//images[5] = door->img;
 	put_sprites(map, game, images);
 }
 
@@ -67,19 +70,22 @@ void	put_sprites(t_map *map, t_game *game, void *images[])
 		j = 0;
 		while (j < map->rows)
 		{
-			mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, images[3], x * 24, y * 24);
 			if (map->data[j][i] == '0')
 				mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, images[1], x * 24, y * 24);
 			else if (map->data[j][i] == '1')
 				mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, images[0], x * 24, y * 24);
 			else if (map->data[j][i] == 'P')
 				mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, images[4], x * 24, y * 24);
-			else if (map->data[j][i] == 'C')
-				mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, images[2], x * 24, y * 24);
+			//else if (map->data[j][i] == 'E')
+				//mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, images[3], x * 24, y * 24);
+			//else if (map->data[i][j] == 'E')
+				//mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, images[5], x * 24, y * 24);
 			//else if (map->data[i][j] == 'C')
 				//mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, images[2], x * 31, y * 31);
 			//else if (map->data[i][j] == 'E')
 				//mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, images[3], x * 31, y * 31);
+					//	mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, images[3], x * 24, y * 24);
+
 			y++;
 			j++;
 		}
