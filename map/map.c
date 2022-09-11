b0d57e38fa7a4bc1551d5d688481aeaae598fb4a
mycaelli <mycaelli@usp.br>
2022-09-12 00:42:33 +0200

    map generate memory leak corrected && .h file updated

--- map/map.c
+++ map/map.c
@@ -6,13 +6,25 @@
 /*   By: mcerquei <mcerquei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 05:42:21 by mcerquei          #+#    #+#             */
-/*   Updated: 2022/09/11 23:32:18 by mcerquei         ###   ########.fr       */
+/*   Updated: 2022/09/12 00:41:28 by mcerquei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "map.h"
 
+int	map_open(char*file_path)
+{
+	int			fd;
+
+	fd = open(file_path, O_RDONLY); //CLOSE FILES
+	if (fd < 0)
+	{
+		ft_printf("Error\n %s\n", strerror(ENOENT));
+		exit(EXIT_FAILURE);
+	}	
+	return (fd);
+}
 
 void	map_size(char *file_path, t_map *map) 
 {
@@ -21,12 +33,7 @@ void	map_size(char *file_path, t_map *map)
 
 	map->rows = 0;
 	map->cols = 0;
-	fd = open(file_path, O_RDONLY); //CLOSE FILES
-	if (fd < 0)
-	{
-		ft_printf("Error\n %s\n", strerror(ENOENT));
-		exit(EXIT_FAILURE);
-	}
+	fd = map_open(file_path);
 	line = get_next_line(fd, 1);
 	while (line[map->cols] && line[map->cols] != '\n')
 		map->cols++;
@@ -38,6 +45,7 @@ void	map_size(char *file_path, t_map *map)
 		map->rows++;
 	}
 }
+
 void	map_initialize(t_map *map)
 {
 	int i;
@@ -49,11 +57,8 @@ void	map_initialize(t_map *map)
 		ft_printf("Error\n %s\n", strerror(ENOMEM));
 		exit (EXIT_FAILURE);
 	}
-	// 0800 726 0101 
-	// opcao 3 e 3
 	while (i < map->rows)
 	{
-		// check if \n is coming too
 		map->data[i] = ft_calloc(sizeof(char*), map->cols);
 		i++;
 	}
@@ -75,24 +80,24 @@ void	map_generate(char *file_path, t_map *map)
 	pos[0] = 0;
 	pos[1] = 0;
 	count = map->rows;
-	fd = open(file_path, O_RDONLY); // ja foi testado
+	fd = map_open(file_path);
 	check = ft_calloc(4, sizeof(int));
 	while (count)
 	{
-		line = get_next_line(fd, 1); //checar se line eh null
+		line = get_next_line(fd, 1);
 		pos[1] = -1;
 		while (pos[1]++ < map->cols)
 		{
 			map->data[pos[0]][pos[1]] = line[pos[1]];
-			//printf("%c", map->data[pos[0]][pos[1]]);
 			check_chars(line[pos[1]], check);
 		}
-		//printf("\n");
+		free(line);
 		pos[0]++;
 		count--;
 	}
 	check_valid_char(check);
 	check_walls(map);
 	free(check);
+	line = get_next_line(fd, 1);
 	free(line);
 }
--- map/map.h
+++ map/map.h
@@ -9,7 +9,11 @@
 # include <stdio.h> // APAGAAAAR
 # include <fcntl.h>
 #include "../libft/libft.h"
-#include "../window/window.h"
+#include <stdlib.h>
+#include <stddef.h> //size_t ?
+#include <mlx.h>
+#include <string.h>
+#include <errno.h>
 
 typedef struct s_map
 {
@@ -24,5 +28,7 @@ void	map_generate(char *file_path, t_map *map);
 void	check_chars(char c, int *check);
 void	check_valid_char(int *check);
 void	check_walls(t_map *map);
+void	map_size(char *file_path, t_map *map);
+int		map_open(char*file_path);
 
 #endif
